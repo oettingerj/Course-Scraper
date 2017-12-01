9e6487974ac1c3071157ddf7644dc9d12fca172a
Josh Oettinger <oettingerj@carleton.edu>
2017-12-01 14:10:04 -0600

    More setup

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 var scraper = require('cheerio');
+var request = require('request');
+
+request('https://apps.carleton.edu/campus/registrar/schedule/enroll/?term=18WI&subject=ECON', function (error, response, html) {
+  if (!error && response.statusCode == 200) {
+    var $ = scraper.load(html);
+  }
+});
